ca076bd78c1b03e9c9e533c737fb7f26895b2c41
youssefGitHub99 <44686746+youssefGitHub99@users.noreply.github.com>
2022-04-25 18:21:41 +0200

    show all authors for the book

--- src/App.js
+++ src/App.js
@@ -1,58 +1,76 @@
-import React from "react"
-import { Route } from "react-router-dom"
+import React from "react";
+import { Route } from "react-router-dom";
 // import * as BooksAPI from './BooksAPI'
-import "./App.css"
-import Main from "./pages/Main"
-import Search from "./pages/Search"
-import * as BooksAPI from "./BooksAPI"
+import "./App.css";
+import Main from "./pages/Main";
+import Search from "./pages/Search";
+import * as BooksAPI from "./BooksAPI";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    books:{},
-    selectedBookAndItsShelf: {selectedBook: {}, shelf: "none"},
-   
-  }
+	state = {
+		/**
+		 * TODO: Instead of using this state variable to keep track of which page
+		 * we're on, use the URL in the browser's address bar. This will ensure that
+		 * users can use the browser's back and forward buttons to navigate between
+		 * pages, as well as provide a good URL they can bookmark and share.
+		 */
+		books: {},
+		selectedBookAndItsShelf: { selectedBook: {}, shelf: "none" },
+	};
 
-  changeShelfOfBook = (selectedBook, shelf) => {
-    
-    BooksAPI.update(selectedBook, shelf)
-    .then(res => console.log("res",res))
-    .catch(err => console.log(err.message));
-    
-    this.setState({selectedBookAndItsShelf:{selectedBook, shelf}});
+	changeShelfOfBook = (selectedBook, shelf) => {
+		BooksAPI.update(selectedBook, shelf)
+			.then(res => console.log("res", res))
+			.catch(err => console.log(err.message));
 
-  }
-  mainSendBookSToSearch = (currentlyReadingBooks, wantToReadBooks, readBooks) => {
-    const tempBooks = {};
-    currentlyReadingBooks.forEach(book => {
-      tempBooks[book.id] = book;
-    });
-    wantToReadBooks.forEach(book => {
-      tempBooks[book.id] = book;
-    });
-    readBooks.forEach(book => {
-      tempBooks[book.id] = book;
-    });
-    this.setState({books: tempBooks})
-  }
+		this.setState({ selectedBookAndItsShelf: { selectedBook, shelf } });
+	};
+	mainSendBookSToSearch = (
+		currentlyReadingBooks,
+		wantToReadBooks,
+		readBooks
+	) => {
+		const tempBooks = {};
+		currentlyReadingBooks.forEach(book => {
+			tempBooks[book.id] = book;
+		});
+		wantToReadBooks.forEach(book => {
+			tempBooks[book.id] = book;
+		});
+		readBooks.forEach(book => {
+			tempBooks[book.id] = book;
+		});
+		this.setState({ books: tempBooks });
+	};
 
-  render() {
-    
-    return (
-      <div className="app">
-
-        <Route exact path="/" render={({history}) => <Main selectedBookAndItsShelf={this.state.selectedBookAndItsShelf} changeShelfOfBook={this.changeShelfOfBook} mainSendBookSToSearch={this.mainSendBookSToSearch} history={history} />}/>
-        <Route path="/search" render={({history}) => <Search changeShelfOfBook={this.changeShelfOfBook} mainBooks={this.state.books} history={history} />}/>
-
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="app">
+				<Route
+					exact
+					path="/"
+					render={({ history }) => (
+						<Main
+							selectedBookAndItsShelf={this.state.selectedBookAndItsShelf}
+							changeShelfOfBook={this.changeShelfOfBook}
+							mainSendBookSToSearch={this.mainSendBookSToSearch}
+							history={history}
+						/>
+					)}
+				/>
+				<Route
+					path="/search"
+					render={({ history }) => (
+						<Search
+							changeShelfOfBook={this.changeShelfOfBook}
+							mainBooks={this.state.books}
+							history={history}
+						/>
+					)}
+				/>
+			</div>
+		);
+	}
 }
 
-export default BooksApp
+export default BooksApp;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,141 +1,163 @@
-import React, {Component} from "react"
-import ListOfBooks from "../components/ListOfBooks"
-import * as BooksAPI from "../BooksAPI"
+import React, { Component } from "react";
+import ListOfBooks from "../components/ListOfBooks";
+import * as BooksAPI from "../BooksAPI";
 
 class Main extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			bookShelves: {
+				currentlyReading: [],
+				wantToRead: [],
+				read: [],
+			},
+			isFetched: false,
+			showError: false,
+		};
+	}
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        bookShelves: {
-          "currentlyReading": [],
-          "wantToRead": [],
-          "read": [] 
-        },
-        isFetched: false,
-        showError: false
-      }
-    }
+	componentDidUpdate(prevProps) {
+		if (
+			prevProps.selectedBookAndItsShelf !== this.props.selectedBookAndItsShelf
+		) {
+			const { selectedBook, shelf } = this.props.selectedBookAndItsShelf;
+			console.log(selectedBook, shelf);
 
-    componentDidUpdate(prevProps){
-      
-      if(prevProps.selectedBookAndItsShelf !== this.props.selectedBookAndItsShelf){
-        
-        const {selectedBook, shelf} = this.props.selectedBookAndItsShelf;
-        console.log(selectedBook, shelf);
+			if (selectedBook.shelf !== shelf) {
+				if (shelf === "none") {
+					const listOfBooksWithoutSelectedBook = this.state.bookShelves[
+						selectedBook.shelf
+					].filter(book => {
+						return book.id !== selectedBook.id;
+					});
 
-        if(selectedBook.shelf !== shelf){
+					this.setState({
+						bookShelves: {
+							...this.state.bookShelves,
+							[selectedBook.shelf]: listOfBooksWithoutSelectedBook,
+						},
+					});
 
-          if(shelf === "none"){
+					return;
+				}
 
-            const listOfBooksWithoutSelectedBook = this.state.bookShelves[selectedBook.shelf].filter(book => {
-              return book.id !== selectedBook.id
-            });
+				if (selectedBook.shelf !== "none") {
+					const listOfBooksWithoutSelectedBook = this.state.bookShelves[
+						selectedBook.shelf
+					].filter(book => {
+						return book.id !== selectedBook.id;
+					});
+					const tempShelf = selectedBook.shelf;
+					selectedBook.shelf = shelf;
 
-            this.setState({bookShelves: {...this.state.bookShelves, [selectedBook.shelf]: listOfBooksWithoutSelectedBook, 
-            }})
+					const listOfBooksWithSelectedBook = this.state.bookShelves[shelf];
+					listOfBooksWithSelectedBook.push(selectedBook);
 
-            return;
-          }
+					this.setState({
+						bookShelves: {
+							...this.state.bookShelves,
+							[tempShelf]: listOfBooksWithoutSelectedBook,
+							[shelf]: listOfBooksWithSelectedBook,
+						},
+					});
+				}
+			}
+		}
+	}
 
-          if(selectedBook.shelf !== "none"){
+	componentDidMount() {
+		BooksAPI.getAll()
+			.then(books => {
+				const currentlyReadingBooks = [];
+				const wantToReadBooks = [];
+				const readBooks = [];
 
-            const listOfBooksWithoutSelectedBook = this.state.bookShelves[selectedBook.shelf].filter(book => {
-              return book.id !== selectedBook.id
-            });
-            const tempShelf = selectedBook.shelf;
-            selectedBook.shelf = shelf;
+				books.forEach(book => {
+					switch (book.shelf) {
+						case "currentlyReading":
+							currentlyReadingBooks.push(book);
+							break;
+						case "wantToRead":
+							wantToReadBooks.push(book);
+							break;
+						default:
+							readBooks.push(book);
+							break;
+					}
+				});
 
-            const listOfBooksWithSelectedBook = this.state.bookShelves[shelf];
-            listOfBooksWithSelectedBook.push(selectedBook);
-            
-            this.setState({bookShelves: {...this.state.bookShelves ,[tempShelf]: listOfBooksWithoutSelectedBook, 
-              [shelf]: listOfBooksWithSelectedBook
-            }});
+				this.setState({
+					bookShelves: {
+						currentlyReading: currentlyReadingBooks,
+						wantToRead: wantToReadBooks,
+						read: readBooks,
+					},
+					showError: false,
+				});
 
-          }
-        }
-      }
-    }
+				this.props.mainSendBookSToSearch(
+					currentlyReadingBooks,
+					wantToReadBooks,
+					readBooks
+				);
+			})
+			.catch(error => {
+				this.setState({ showError: true });
+			});
+	}
 
-    componentDidMount(){
-      
-      BooksAPI.getAll().then(books => {
-        const currentlyReadingBooks = [];
-        const wantToReadBooks = [];
-        const readBooks = [];
-       
-        books.forEach(book => {
-          switch (book.shelf) {
-            case "currentlyReading":
-              currentlyReadingBooks.push(book);
-              break;
-            case "wantToRead":
-              wantToReadBooks.push(book);
-              break;
-            default:
-              readBooks.push(book);
-              break;
-          }
-        });
+	render() {
+		return (
+			<div className="list-books">
+				<div className="list-books-title">
+					<h1>MyReads</h1>
+				</div>
 
-        this.setState({
-          bookShelves: {
-            "currentlyReading": currentlyReadingBooks,
-            "wantToRead": wantToReadBooks,
-            "read": readBooks 
-          },
-          showError: false
-        });
-
-        this.props.mainSendBookSToSearch(currentlyReadingBooks, wantToReadBooks, readBooks);
-
-      }).catch(error => {
-        this.setState({showError: true})
-      })
-    }
-  
-    render(){
-
-        return (
-          <div className="list-books">
-
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-
-            {this.state.showError? <p>check connection</p>: <>
-              <div className="list-books-content">
-                <div>
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                    <ListOfBooks changeShelfOfBook={this.props.changeShelfOfBook}  books= {this.state.bookShelves["currentlyReading"]}/>
-                    </div>
-                  </div>
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Want to Read</h2>
-                    <div className="bookshelf-books">
-                    <ListOfBooks changeShelfOfBook={this.props.changeShelfOfBook} books= {this.state.bookShelves["wantToRead"]}/>
-                    </div>
-                  </div>
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Read</h2>
-                    <div className="bookshelf-books">
-                      <ListOfBooks changeShelfOfBook={this.props.changeShelfOfBook} books= {this.state.bookShelves["read"]}/>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="open-search">
-                <button onClick={() => this.props.history.push("/search")}>Add a book</button>
-              </div>
-            </>
-            }
-          </div>
-        )
-    }
-
+				{this.state.showError ? (
+					<p>check connection</p>
+				) : (
+					<>
+						<div className="list-books-content">
+							<div>
+								<div className="bookshelf">
+									<h2 className="bookshelf-title">Currently Reading</h2>
+									<div className="bookshelf-books">
+										<ListOfBooks
+											changeShelfOfBook={this.props.changeShelfOfBook}
+											books={this.state.bookShelves["currentlyReading"]}
+										/>
+									</div>
+								</div>
+								<div className="bookshelf">
+									<h2 className="bookshelf-title">Want to Read</h2>
+									<div className="bookshelf-books">
+										<ListOfBooks
+											changeShelfOfBook={this.props.changeShelfOfBook}
+											books={this.state.bookShelves["wantToRead"]}
+										/>
+									</div>
+								</div>
+								<div className="bookshelf">
+									<h2 className="bookshelf-title">Read</h2>
+									<div className="bookshelf-books">
+										<ListOfBooks
+											changeShelfOfBook={this.props.changeShelfOfBook}
+											books={this.state.bookShelves["read"]}
+										/>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="open-search">
+							<button onClick={() => this.props.history.push("/search")}>
+								Add a book
+							</button>
+						</div>
+					</>
+				)}
+			</div>
+		);
+	}
 }
 
-export default Main
+export default Main;
